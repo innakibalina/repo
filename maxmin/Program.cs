8ed1aa8b4b3de100f546f143922e8cc244cf52ba
Inna <inna.kibalina@gmail.com>
2022-10-20 13:40:08 +0700

    Исправлено решение 1 задачи

--- maxmin/Program.cs
+++ maxmin/Program.cs
@@ -1,16 +1,21 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-//a = 5; b = 7 -> max = 7
 
 Console.WriteLine("Какое число большее, какое меньшее?");
 Console.WriteLine("Введите число a: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число b: ");
 int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
 if (a > b)
 {
-Console.WriteLine("Число a - максимальное, b - минимальное");
+   Console.WriteLine("max = " + a);
+   Console.WriteLine("min = " + b);
+
 }
     else {
-        Console.WriteLine("Число b - максимальное, a - минимальное");
+     Console.WriteLine("max = " + b);
+     Console.WriteLine("min = " + a);
     }
