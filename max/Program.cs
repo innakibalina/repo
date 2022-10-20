c5ad0b1d8d70663cca5aeb7d6993e8dd39dbb1b3
Inna <inna.kibalina@gmail.com>
2022-10-20 13:33:13 +0700

    Задача 2 готова

--- max/Program.cs
+++ max/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("1111Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Какое число наибольшее из трех?");
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число с: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max=a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.WriteLine("Наибольшее число: " + max);
+
+
+
+        
